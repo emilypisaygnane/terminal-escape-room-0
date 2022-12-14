9aa09d77938d96c579fbe8f5e5266716a1ed8549
Erik M Baxstrom <baxstrem@gmail.com>
2022-12-13 16:50:56 -0800

    basic terminal functionality implemented -- get and display prompts and actions, select action

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,53 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-// const inquirer = require('inquirer');
+const dotenv = require('dotenv');
+dotenv.config();
+const inquirer = require('inquirer');
+const Prompt = require('../lib/models/Prompt.js');
 const { getPromptById } = require('./utils/fetch-utils');
 
 console.log('hello world!');
-getPromptById(1);
+// getPromptById(1);
 
-const response = {
-  description: 'you look around...',
-  actions: [
-    { description: 'examine window', nextPrompt: 2 },
-    { description: 'examine desk', nextPrompt: 4 },
-  ],
+// const something = await getPromptById(1);
+// console.log(something);
+
+const start = async () => {
+  let currentPrompt = 1;
+  while (currentPrompt) {
+    // const response = await getPromptById(currentPrompt);
+    const response = await Prompt.getById(currentPrompt);
+    //display the prompt and options, get the user input
+    const answers = await inquirer.prompt({
+      prefix: '*',
+      type: 'list',
+      message: response.description,
+      name: `prompt ${response.id}`,
+      choices: response.actions.map((action) => ({
+        name: action.description,
+        value: action.id,
+      })),
+    });
+    const chosenAction = response.actions.find((action) => {
+      return action.id === answers[`prompt ${response.id}`];
+    });
+    currentPrompt = chosenAction.next_prompt_id;
+    console.log(currentPrompt);
+  }
 };
+start();
+
+// const response = {
+//   description: 'you look around...',
+//   actions: [
+//     { description: 'examine window', nextPrompt: 2 },
+//     { description: 'examine desk', nextPrompt: 4 },
+//   ],
+// };
+
+// let stage = [1];
+// const story = async (stage) => {
+//   const { description, actions } = await getPromptById(stage[0]);
+//   return { description, actions };
+// };
+// story();
