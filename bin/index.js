181f195556ec255ff292e7f8528ddfcf5dc07b06
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-14 10:59:59 -0800

    working on logic

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const inquirer = require('inquirer');
 const Prompt = require('../lib/models/Prompt.js');
 const actionState = require('../lib/models/actionState.js');
+const userState = require('../lib/models/userState.js');
 
 const start = async () => {
   let currentPrompt = 1;
@@ -26,7 +27,14 @@ const start = async () => {
       return action.id === answers[`prompt ${response.id}`];
     });
     // set current action to false
-    // 
+    
+    // This would allow you to specify actions of type update_state in the conversation tree, which would update the user's state 
+    //with the new values provided in the action's state property. This could be useful for tracking information about the user's 
+    //progress through the conversation, or for storing information that the user has provided in previous prompts.
+    if (chosenAction.type === 'update_state') {
+      // update the user's state with the new values provided in chosenAction.state
+      userState = { ...userState, ...chosenAction.state };
+    }
     
     currentPrompt = chosenAction.next_prompt_id;
     
