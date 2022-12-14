07713d7d7b5db96bd1224a267c438672fb6b514f
Tanner Richards <Tanner.richards990@gmail.com>
2022-12-13 21:48:59 -0800

    Got go back feature working, fixed tables and queries, working towards new logic. EP, EK, EB, TR

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const inquirer = require('inquirer');
 const Prompt = require('../lib/models/Prompt.js');
+const actionState = require('../lib/models/actionState.js');
+
 
 
 
@@ -29,10 +31,20 @@ const start = async () => {
     const chosenAction = response.actions.find((action) => {
       return action.id === answers[`prompt ${response.id}`];
     });
+    // set current action to false
+    // 
+    
     currentPrompt = chosenAction.next_prompt_id;
+    
+  
     console.log(currentPrompt);
+
   }
+
 };
 start();
 
-
+// SQL QUERY FOR JOINING ACTION_STATE AND USER_STATE
+// SELECT COUNT (action_state.state_id) as action_count, COUNT (user_state.state_id) as user_count 
+// FROM action_state LEFT JOIN user_state ON action_state.state_id = user_state.state_id
+// WHERE action_state.action_id = 11
